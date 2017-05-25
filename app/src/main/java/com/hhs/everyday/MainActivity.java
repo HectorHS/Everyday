a313062fe364436831e95c7710d86df239375b62
HectorHS <kelanved@gmail.com>
2017-05-25 17:42:10 +0100

    simple listview for activities

--- app/src/main/java/com/hhs/everyday/MainActivity.java
+++ app/src/main/java/com/hhs/everyday/MainActivity.java
@@ -2,6 +2,9 @@ package com.hhs.everyday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Simple array with a list of my favorite TV shows
+        String[] favoriteTVShows = {"Pushing Daisies", "Better Off Ted",
+                "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead",
+                "Breaking Bad", "The 400", "Alphas", "Life on Mars"};
+        // The ListAdapter acts as a bridge between the data and each ListItem
+        // You fill the ListView with a ListAdapter. You pass it a context represented by
+        // this. A Context provides access to resources you need.
+        // android.R.layout.simple_list_item_1 is one of the resources needed.
+        // It is a predefined layout provided by Android that stands in as a default
+
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, R.layout.task_row_layout, R.id.textView1, favoriteTVShows);
+
+        // ListViews display data in a scrollable list
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+
+        // Tells the ListView what data to use
+        theListView.setAdapter(theAdapter);
+
     }
 }
